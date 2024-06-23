e7917a3cd07619941ff52906ec5d09ce03ae4011
Nahid HK <nahidappltd@gmail.com>
2024-06-23 18:19:12 +0600

    new update

---
+++ app.js
@@ -0,0 +1,10 @@
+function serchapp() {
+    document.getElementById("navsid").style.display = "block"
+}
+function back() {
+    document.getElementById("navsid").style.display = "none"
+}
+
+function userlist(){
+    document.getElementById("pop1").style.display="block"
+}
